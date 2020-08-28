7247eb8040964ce01566a6878d402521af5a1c27
abhijeet3922 <abhijeet.kumar@fmr.com>
2020-08-28 13:51:45 +0000

    updating py files

--- setup.py
+++ setup.py
@@ -2,23 +2,32 @@ import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+    
+with open("requirements.txt") as fh:
+    required = fh.read().splitlines()
 
 setuptools.setup(
     name="classitransformers",
     packages=['classitransformers'],
     version="0.0.1",
+    author="FMR LLC",
+    author_email="classitransformers@fmr.com",
     description="An abstract library for implementing text classification tasks based on various transformers based language models",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/fidelity/classitransformers",
-
+    packages=setuptools.find_packages(),
+    install_requires=required,
+    python_requires='>=3.6',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: Apache License 2.0",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
     keywords='NLP language_models text_classification bert electra roberta distilbert albert',
-    author='FMR LLC',
+    
+    project_urls={
+        "Source": "https://github.com/fidelity/classitransformers"
+    }
 )
 
